6bc88446814c32959842b1cbdec095704c66eb74
Galen Elias <gelias@gmail.com>
2015-04-29 02:33:33 -0700

    Minor emulation accuracy fix for controller port reads.

--- SaltyLib/SaltyLib.Shared/Controller.cpp
+++ SaltyLib/SaltyLib.Shared/Controller.cpp
@@ -23,6 +23,10 @@ void Controller::WriteData(uint8_t value)
 uint8_t Controller::ReadData()
 {
 	uint8_t result = m_readInputOffset < (int)ControllerInput::_Max ? m_inputs[m_readInputOffset] : 0;
+	
+	// The top bits of the data aren't set by the controller, so should retain the previous bytes of the bus
+	// which is generally 40 since that is the address of the controller IO memory mapped addresses.
+	result |= 0x40;
 
 	if (!m_strobeOn)
 	{
